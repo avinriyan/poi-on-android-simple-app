8a3a9bf79fb2b74e709f09dde756355c8f13f3fc
Avin Riyan <avinriyant@gmail.com>
2020-07-12 17:45:53 +0700

    Remove Anko library
    Anko is deprecated.

--- app/src/main/java/com/larapin/poiandroid/MainActivity.kt
+++ app/src/main/java/com/larapin/poiandroid/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
         System.setProperty("org.apache.poi.javax.xml.stream.XMLInputFactory", "com.fasterxml.aalto.stax.InputFactoryImpl")
         System.setProperty("org.apache.poi.javax.xml.stream.XMLOutputFactory", "com.fasterxml.aalto.stax.OutputFactoryImpl")
         System.setProperty("org.apache.poi.javax.xml.stream.XMLEventFactory", "com.fasterxml.aalto.stax.EventFactoryImpl")
